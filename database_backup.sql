60d942611ce818214e59f53525da0955ace83fc6
Auto Backup <auto-backup@replit.com>
2026-03-04 20:12:04 +0000

    Auto backup [startup] 2026-03-04 20:12

--- database_backup.sql
+++ database_backup.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict PL476zaP0mexk91ovyDDXpBczvgAakM3bNJdpYzl8B3FhF5MBCmqImn8RrifsDa
+\restrict rD47nCaVIBiYzgiJGVQjgu49eqPSVgw76lZNPd9hUkYwJKfAted0CdOzm1YaF69
 
 -- Dumped from database version 16.10
 -- Dumped by pg_dump version 16.10
@@ -4115,5 +4115,5 @@ ALTER TABLE ONLY public.work_orders
 -- PostgreSQL database dump complete
 --
 
-\unrestrict PL476zaP0mexk91ovyDDXpBczvgAakM3bNJdpYzl8B3FhF5MBCmqImn8RrifsDa
+\unrestrict rD47nCaVIBiYzgiJGVQjgu49eqPSVgw76lZNPd9hUkYwJKfAted0CdOzm1YaF69
 
