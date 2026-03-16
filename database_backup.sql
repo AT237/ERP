9ef370f388c7b17359bf0ca976fbba945b91c0d3
Auto Backup <auto-backup@replit.com>
2026-03-16 11:23:24 +0000

    Auto backup [startup] 2026-03-16 11:23

--- database_backup.sql
+++ database_backup.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict pvxaej7GBjCLkBFa741UNaZe8uJBPVRPgvwTxgv4t47TBwX8USNQJWIXlgfTjBQ
+\restrict oWhgYAhGb3f3tzfLGUkeqMF5QZWm3QE3Uu29d2uQ4Fpw9AnM7JrdLkaajjEQm5j
 
 -- Dumped from database version 16.10
 -- Dumped by pg_dump version 16.10
@@ -3334,7 +3334,7 @@ bffa71e4-59b0-47a4-9d35-59992507a495	\N	\N	\N	2026-03-02 18:55:11.019526	2026-03
 COPY public.user_sessions (sid, sess, expire) FROM stdin;
 JpAWRc0NRtFxRpb0s2rmZsswzN68USAj	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-21T07:33:22.414Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-03-21 07:33:23
 dDrAtdLR1g2KLYQMwkwIVP-nr2ZmpGRT	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-22T09:01:24.133Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-03-22 09:01:25
-oEmieC_lXuDng36uYwBtCrmu_Lvml_oH	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-23T10:41:41.532Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-03-23 11:19:57
+oEmieC_lXuDng36uYwBtCrmu_Lvml_oH	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-23T10:41:41.532Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-03-23 11:22:37
 \.
 
 
@@ -4653,5 +4653,5 @@ ALTER TABLE ONLY public.work_orders
 -- PostgreSQL database dump complete
 --
 
-\unrestrict pvxaej7GBjCLkBFa741UNaZe8uJBPVRPgvwTxgv4t47TBwX8USNQJWIXlgfTjBQ
+\unrestrict oWhgYAhGb3f3tzfLGUkeqMF5QZWm3QE3Uu29d2uQ4Fpw9AnM7JrdLkaajjEQm5j
 
