09f0d84114db2fe72fa0b065ab0767aea8d7934b
Auto Backup <auto-backup@replit.com>
2026-03-23 14:30:05 +0000

    Auto backup [startup] 2026-03-23 14:30

--- database_backup.sql
+++ database_backup.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict WYdkjYTmKNZedKEyhFcDEZscdX8YoS0T86sBvKYFHwcZ4qHtewDGlenyx3Lsl28
+\restrict lpJuDKObzOGsYfXGiNNOCMHChdRet6OUt48euKgWu38B9buxYnAol2wLWygIkiu
 
 -- Dumped from database version 16.10
 -- Dumped by pg_dump version 16.10
@@ -3371,8 +3371,8 @@ COPY public.user_sessions (sid, sess, expire) FROM stdin;
 oypZfvgkL_zW8_gElfJoYHlAoOn9Krxy	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-30T12:45:11.462Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-03-30 12:45:12
 G-hpK55bj_sJ0KetmjY_RDon7mSC8vNL	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-30T13:24:39.977Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-03-30 13:24:41
 HA9sx8HgSCwNfLzZ25Rs-zot8Jz-5Xmc	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-30T12:49:50.966Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-03-30 12:49:52
-yfGCzjUSFmdjjFzA1bMksjG1HAFhXRmX	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-30T12:21:35.460Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-03-30 14:27:14
 oEmieC_lXuDng36uYwBtCrmu_Lvml_oH	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-23T10:41:41.532Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-03-30 09:11:01
+yfGCzjUSFmdjjFzA1bMksjG1HAFhXRmX	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-30T12:21:35.460Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-03-30 14:29:19
 brOBMLpZ3lvlr-61KcpCFD9Ju20B-_Rd	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-30T12:54:15.680Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-03-30 12:54:16
 66kYKTeLVvLxBGlMp-Qssl-W6tsmNEBE	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-30T13:33:21.594Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-03-30 13:33:22
 \.
@@ -4717,5 +4717,5 @@ ALTER TABLE ONLY public.work_orders
 -- PostgreSQL database dump complete
 --
 
-\unrestrict WYdkjYTmKNZedKEyhFcDEZscdX8YoS0T86sBvKYFHwcZ4qHtewDGlenyx3Lsl28
+\unrestrict lpJuDKObzOGsYfXGiNNOCMHChdRet6OUt48euKgWu38B9buxYnAol2wLWygIkiu
 
