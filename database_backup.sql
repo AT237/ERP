f7afd4a96dc550ffb5e6d73b57187465f0a575cd
Auto Backup <auto-backup@replit.com>
2026-03-27 21:39:08 +0000

    Auto backup [startup] 2026-03-27 21:39

--- database_backup.sql
+++ database_backup.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict UGc6EwTaUq1WNN3NolKNihFmsKR7b8BiNRreUNxVDA8yO4GCNyDMnO5E38gIkBu
+\restrict QwXkAmzjx7irYc3XVkCrgbm5QhdEdeobrpLPOT11ck0LN7EzOFL4YepU5R012vN
 
 -- Dumped from database version 16.10
 -- Dumped by pg_dump version 16.10
@@ -3386,8 +3386,8 @@ DydfCmxnF9Y7M8cXrMNtollnPcwzY2Eh	{"cookie":{"originalMaxAge":604800000,"expires"
 PA3DwNha04cB0K8L_9gpEk09h3fue6uM	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-03T10:05:01.379Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-03 10:05:02
 06tRqKlA-rfQ_GqAPUsO6r6LmD4Rv0Wf	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-03T10:03:38.029Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-03 10:03:39
 oEmieC_lXuDng36uYwBtCrmu_Lvml_oH	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-23T10:41:41.532Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-03-30 09:11:01
-yfGCzjUSFmdjjFzA1bMksjG1HAFhXRmX	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-30T12:21:35.460Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-03 21:36:03
 brOBMLpZ3lvlr-61KcpCFD9Ju20B-_Rd	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-30T12:54:15.680Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-03-30 12:54:16
+yfGCzjUSFmdjjFzA1bMksjG1HAFhXRmX	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-30T12:21:35.460Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-03 21:38:22
 66kYKTeLVvLxBGlMp-Qssl-W6tsmNEBE	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-30T13:33:21.594Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-03-30 13:33:22
 \.
 
@@ -4731,5 +4731,5 @@ ALTER TABLE ONLY public.work_orders
 -- PostgreSQL database dump complete
 --
 
-\unrestrict UGc6EwTaUq1WNN3NolKNihFmsKR7b8BiNRreUNxVDA8yO4GCNyDMnO5E38gIkBu
+\unrestrict QwXkAmzjx7irYc3XVkCrgbm5QhdEdeobrpLPOT11ck0LN7EzOFL4YepU5R012vN
 
