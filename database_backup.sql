4bba8ee0c03da37b08e85af901a7dd8085145b44
Auto Backup <auto-backup@replit.com>
2026-03-27 21:02:20 +0000

    Auto backup [startup] 2026-03-27 21:02

--- database_backup.sql
+++ database_backup.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict 9dWTubdCcxcOzoan8z1jnhNah7hv823gF5seDQheia3qAtvqc308d3VuqU3GzwT
+\restrict xEeyJOCU3w6Xf1IOc4rRGFmIw24I3gN5mwC8nuMWDjOMdkvrSA23DElttyQy3lW
 
 -- Dumped from database version 16.10
 -- Dumped by pg_dump version 16.10
@@ -3381,7 +3381,7 @@ COPY public.user_sessions (sid, sess, expire) FROM stdin;
 oypZfvgkL_zW8_gElfJoYHlAoOn9Krxy	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-30T12:45:11.462Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-03-30 12:45:12
 MzJWnBVuaC5erqfSfjQ8izZaZSq-9k83	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-03T10:01:54.937Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-03 10:01:56
 G-hpK55bj_sJ0KetmjY_RDon7mSC8vNL	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-30T13:24:39.977Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-03-30 13:24:41
-yfGCzjUSFmdjjFzA1bMksjG1HAFhXRmX	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-30T12:21:35.460Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-03 20:59:23
+yfGCzjUSFmdjjFzA1bMksjG1HAFhXRmX	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-30T12:21:35.460Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-03 21:01:35
 HA9sx8HgSCwNfLzZ25Rs-zot8Jz-5Xmc	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-30T12:49:50.966Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-03-30 12:49:52
 DydfCmxnF9Y7M8cXrMNtollnPcwzY2Eh	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-03T10:02:00.267Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-03 10:02:41
 PA3DwNha04cB0K8L_9gpEk09h3fue6uM	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-03T10:05:01.379Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-03 10:05:02
@@ -4731,5 +4731,5 @@ ALTER TABLE ONLY public.work_orders
 -- PostgreSQL database dump complete
 --
 
-\unrestrict 9dWTubdCcxcOzoan8z1jnhNah7hv823gF5seDQheia3qAtvqc308d3VuqU3GzwT
+\unrestrict xEeyJOCU3w6Xf1IOc4rRGFmIw24I3gN5mwC8nuMWDjOMdkvrSA23DElttyQy3lW
 
