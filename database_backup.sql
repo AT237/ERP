11039ed652b28cf83c5e24ecd427b2ce8abbcace
Auto Backup <auto-backup@replit.com>
2026-03-29 18:32:06 +0000

    Auto backup [startup] 2026-03-29 18:32

--- database_backup.sql
+++ database_backup.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict fQZ5UEqFGAKhTGJ9XvJW51QRDN2zvOBmioSvx7tGWdxes33o6jxHsTm2JrcobVZ
+\restrict l0iQCn6gOhVfxRvEidEq3g92EqlWB1yr3WDzqU75XWVTnGcScQfd13f1XICNejb
 
 -- Dumped from database version 16.10
 -- Dumped by pg_dump version 16.10
@@ -3437,7 +3437,7 @@ PA3DwNha04cB0K8L_9gpEk09h3fue6uM	{"cookie":{"originalMaxAge":604800000,"expires"
 oEmieC_lXuDng36uYwBtCrmu_Lvml_oH	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-23T10:41:41.532Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-03-30 09:11:01
 brOBMLpZ3lvlr-61KcpCFD9Ju20B-_Rd	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-30T12:54:15.680Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-03-30 12:54:16
 66kYKTeLVvLxBGlMp-Qssl-W6tsmNEBE	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-30T13:33:21.594Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-03-30 13:33:22
-yfGCzjUSFmdjjFzA1bMksjG1HAFhXRmX	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-30T12:21:35.460Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-04 22:23:46
+yfGCzjUSFmdjjFzA1bMksjG1HAFhXRmX	{"cookie":{"originalMaxAge":604800000,"expires":"2026-03-30T12:21:35.460Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-05 18:31:15
 \.
 
 
@@ -4804,5 +4804,5 @@ ALTER TABLE ONLY public.work_orders
 -- PostgreSQL database dump complete
 --
 
-\unrestrict fQZ5UEqFGAKhTGJ9XvJW51QRDN2zvOBmioSvx7tGWdxes33o6jxHsTm2JrcobVZ
+\unrestrict l0iQCn6gOhVfxRvEidEq3g92EqlWB1yr3WDzqU75XWVTnGcScQfd13f1XICNejb
 
