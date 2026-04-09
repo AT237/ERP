813ec1ae1fdb6b4897ef475d5ae7ad4e98ea6b85
Auto Backup <auto-backup@replit.com>
2026-04-09 10:04:29 +0000

    Auto backup [startup] 2026-04-09 10:04

--- database_backup.sql
+++ database_backup.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict y0FuoVmFcUtf0sArKzkjFA9WydVr46OF11ZfP28fY0NkDN781mg9PbY4LqBgY1a
+\restrict cKY1d6IxaWWcQdTGnKqK8A0n6oxlL9JXRlDtumfXWjUmI1sy8YLgg6fLP7jk3kc
 
 -- Dumped from database version 16.10
 -- Dumped by pg_dump version 16.10
@@ -3686,8 +3686,8 @@ COPY public.user_sessions (sid, sess, expire) FROM stdin;
 8geNcP-CMKiED6tl9n4DTFQeWfxStevQ	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-10T11:42:23.985Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-10 11:42:25
 CHT3r4La7hWEYODSLhXsmnp1QNmnOh4L	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-10T11:42:30.598Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-10 11:42:31
 Y6VX0BCjZ3xsjhAdK4tkCq2FddN57Xgf	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-10T11:42:34.670Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-10 11:42:40
-kWgjWYE7p5KrXFQNZMRwczmcNXiikMuJ	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-13T19:32:02.344Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-16 09:59:12
 Sv76RFsB9y7SL3Pi0yfZSv2polZI6VeU	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-06T12:44:19.702Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-12 18:26:27
+kWgjWYE7p5KrXFQNZMRwczmcNXiikMuJ	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-13T19:32:02.344Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-16 10:03:37
 \.
 
 
@@ -5222,5 +5222,5 @@ ALTER TABLE ONLY public.work_orders
 -- PostgreSQL database dump complete
 --
 
-\unrestrict y0FuoVmFcUtf0sArKzkjFA9WydVr46OF11ZfP28fY0NkDN781mg9PbY4LqBgY1a
+\unrestrict cKY1d6IxaWWcQdTGnKqK8A0n6oxlL9JXRlDtumfXWjUmI1sy8YLgg6fLP7jk3kc
 
