a23f3c3deaff9f382a2a19d39d9f3fb1e89d1789
Auto Backup <auto-backup@replit.com>
2026-04-09 11:44:52 +0000

    Auto backup [startup] 2026-04-09 11:44

--- database_backup.sql
+++ database_backup.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict pc4Q78LPWCsMYBmErspktibedzGUrdr8OXAn8ntxYrGKPBIKzoVEdeAYiptF1X6
+\restrict oEa019iWQspetfnMZjiJ3Rec9kqFlB6e2481W2nDYOqeXbvdTHpWWdr5p6zoXqi
 
 -- Dumped from database version 16.10
 -- Dumped by pg_dump version 16.10
@@ -3692,8 +3692,8 @@ COPY public.user_sessions (sid, sess, expire) FROM stdin;
 8geNcP-CMKiED6tl9n4DTFQeWfxStevQ	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-10T11:42:23.985Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-10 11:42:25
 CHT3r4La7hWEYODSLhXsmnp1QNmnOh4L	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-10T11:42:30.598Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-10 11:42:31
 Y6VX0BCjZ3xsjhAdK4tkCq2FddN57Xgf	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-10T11:42:34.670Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-10 11:42:40
-kWgjWYE7p5KrXFQNZMRwczmcNXiikMuJ	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-13T19:32:02.344Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-16 11:41:43
 Sv76RFsB9y7SL3Pi0yfZSv2polZI6VeU	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-06T12:44:19.702Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-12 18:26:27
+kWgjWYE7p5KrXFQNZMRwczmcNXiikMuJ	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-13T19:32:02.344Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-16 11:44:10
 \.
 
 
@@ -5228,5 +5228,5 @@ ALTER TABLE ONLY public.work_orders
 -- PostgreSQL database dump complete
 --
 
-\unrestrict pc4Q78LPWCsMYBmErspktibedzGUrdr8OXAn8ntxYrGKPBIKzoVEdeAYiptF1X6
+\unrestrict oEa019iWQspetfnMZjiJ3Rec9kqFlB6e2481W2nDYOqeXbvdTHpWWdr5p6zoXqi
 
