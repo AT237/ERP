13fd446b4f32f0e250903adb44606f70e86dc5f2
Auto Backup <auto-backup@replit.com>
2026-04-09 19:35:51 +0000

    Auto backup [startup] 2026-04-09 19:35

--- database_backup.sql
+++ database_backup.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict WWyYogneiXxYUCFDFly6XZ3OVJtqHTcNUdOeVs7VJcCYEmoA0sfh2sPzFalTS8Y
+\restrict BktBwjPzZrCTmiy4OcbQuj8fdEkxv6cZ7GFSYxJx5SrafA2WmOTw4qzue95iqro
 
 -- Dumped from database version 16.10
 -- Dumped by pg_dump version 16.10
@@ -3701,7 +3701,7 @@ COPY public.user_sessions (sid, sess, expire) FROM stdin;
 8geNcP-CMKiED6tl9n4DTFQeWfxStevQ	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-10T11:42:23.985Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-10 11:42:25
 CHT3r4La7hWEYODSLhXsmnp1QNmnOh4L	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-10T11:42:30.598Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-10 11:42:31
 Y6VX0BCjZ3xsjhAdK4tkCq2FddN57Xgf	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-10T11:42:34.670Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-10 11:42:40
-kWgjWYE7p5KrXFQNZMRwczmcNXiikMuJ	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-13T19:32:02.344Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-16 19:33:34
+kWgjWYE7p5KrXFQNZMRwczmcNXiikMuJ	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-13T19:32:02.344Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-16 19:35:06
 Sv76RFsB9y7SL3Pi0yfZSv2polZI6VeU	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-06T12:44:19.702Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-12 18:26:27
 \.
 
@@ -5245,5 +5245,5 @@ ALTER TABLE ONLY public.work_orders
 -- PostgreSQL database dump complete
 --
 
-\unrestrict WWyYogneiXxYUCFDFly6XZ3OVJtqHTcNUdOeVs7VJcCYEmoA0sfh2sPzFalTS8Y
+\unrestrict BktBwjPzZrCTmiy4OcbQuj8fdEkxv6cZ7GFSYxJx5SrafA2WmOTw4qzue95iqro
 
