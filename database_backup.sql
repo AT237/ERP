1a438ffd541b19809de68e8eada4dc570c623b44
Auto Backup <auto-backup@replit.com>
2026-04-09 10:00:04 +0000

    Auto backup [startup] 2026-04-09 10:00

--- database_backup.sql
+++ database_backup.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict GEfoBzILsIhGBpRNwV3elxTpZhLFgBL49qpBCIc1Sn8iIyfcA4LCjP0cFXVLaYI
+\restrict y0FuoVmFcUtf0sArKzkjFA9WydVr46OF11ZfP28fY0NkDN781mg9PbY4LqBgY1a
 
 -- Dumped from database version 16.10
 -- Dumped by pg_dump version 16.10
@@ -3686,7 +3686,7 @@ COPY public.user_sessions (sid, sess, expire) FROM stdin;
 8geNcP-CMKiED6tl9n4DTFQeWfxStevQ	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-10T11:42:23.985Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-10 11:42:25
 CHT3r4La7hWEYODSLhXsmnp1QNmnOh4L	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-10T11:42:30.598Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-10 11:42:31
 Y6VX0BCjZ3xsjhAdK4tkCq2FddN57Xgf	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-10T11:42:34.670Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-10 11:42:40
-kWgjWYE7p5KrXFQNZMRwczmcNXiikMuJ	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-13T19:32:02.344Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-16 09:56:42
+kWgjWYE7p5KrXFQNZMRwczmcNXiikMuJ	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-13T19:32:02.344Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-16 09:59:12
 Sv76RFsB9y7SL3Pi0yfZSv2polZI6VeU	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-06T12:44:19.702Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-12 18:26:27
 \.
 
@@ -5222,5 +5222,5 @@ ALTER TABLE ONLY public.work_orders
 -- PostgreSQL database dump complete
 --
 
-\unrestrict GEfoBzILsIhGBpRNwV3elxTpZhLFgBL49qpBCIc1Sn8iIyfcA4LCjP0cFXVLaYI
+\unrestrict y0FuoVmFcUtf0sArKzkjFA9WydVr46OF11ZfP28fY0NkDN781mg9PbY4LqBgY1a
 
