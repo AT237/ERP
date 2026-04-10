b81213a8f88ec7e33771dc7176ae8370829c835c
Auto Backup <auto-backup@replit.com>
2026-04-10 09:18:31 +0000

    Auto backup [startup] 2026-04-10 09:18

--- database_backup.sql
+++ database_backup.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict 0RJ27jnSBoi1NyEzM1NfveqkNuWOQYLrrsRbqfHtFoJlFudBC2HWOMvlrp5wluR
+\restrict QFqEVxcWiuWR9l9CPdCAwBkeg8BAfb8KUVyTV0Eua8zqDTsrJKSlJA2k57KFdDu
 
 -- Dumped from database version 16.10
 -- Dumped by pg_dump version 16.10
@@ -3904,7 +3904,7 @@ COPY public.user_sessions (sid, sess, expire) FROM stdin;
 8geNcP-CMKiED6tl9n4DTFQeWfxStevQ	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-10T11:42:23.985Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-10 11:42:25
 CHT3r4La7hWEYODSLhXsmnp1QNmnOh4L	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-10T11:42:30.598Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-10 11:42:31
 Y6VX0BCjZ3xsjhAdK4tkCq2FddN57Xgf	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-10T11:42:34.670Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-10 11:42:40
-kWgjWYE7p5KrXFQNZMRwczmcNXiikMuJ	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-13T19:32:02.344Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-17 09:14:59
+kWgjWYE7p5KrXFQNZMRwczmcNXiikMuJ	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-13T19:32:02.344Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-17 09:17:38
 Sv76RFsB9y7SL3Pi0yfZSv2polZI6VeU	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-06T12:44:19.702Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-12 18:26:27
 \.
 
@@ -5448,5 +5448,5 @@ ALTER TABLE ONLY public.work_orders
 -- PostgreSQL database dump complete
 --
 
-\unrestrict 0RJ27jnSBoi1NyEzM1NfveqkNuWOQYLrrsRbqfHtFoJlFudBC2HWOMvlrp5wluR
+\unrestrict QFqEVxcWiuWR9l9CPdCAwBkeg8BAfb8KUVyTV0Eua8zqDTsrJKSlJA2k57KFdDu
 
