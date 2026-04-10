90a0b7f91987d8b1de8f729db65da95a3bcff394
Auto Backup <auto-backup@replit.com>
2026-04-10 11:55:37 +0000

    Auto backup [startup] 2026-04-10 11:55

--- database_backup.sql
+++ database_backup.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict 5x1rXJTbLJuKZLQEzvKhokYWNjE5M5uhWbuBfYO0PWwBdiLKsoEheUjYEp6O7sZ
+\restrict mR2x61wWI9RreHZsEnKa1agx90wtMuKUmdGhoJTkFWl4EZVdM7JcFcLMehivMcS
 
 -- Dumped from database version 16.10
 -- Dumped by pg_dump version 16.10
@@ -3906,10 +3906,10 @@ bffa71e4-59b0-47a4-9d35-59992507a495	\N	\N	\N	2026-03-02 18:55:11.019526	2026-03
 --
 
 COPY public.user_sessions (sid, sess, expire) FROM stdin;
-kWgjWYE7p5KrXFQNZMRwczmcNXiikMuJ	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-13T19:32:02.344Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-17 09:54:49
 8geNcP-CMKiED6tl9n4DTFQeWfxStevQ	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-10T11:42:23.985Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-10 11:42:25
 CHT3r4La7hWEYODSLhXsmnp1QNmnOh4L	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-10T11:42:30.598Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-10 11:42:31
 Y6VX0BCjZ3xsjhAdK4tkCq2FddN57Xgf	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-10T11:42:34.670Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-10 11:42:40
+kWgjWYE7p5KrXFQNZMRwczmcNXiikMuJ	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-13T19:32:02.344Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-17 11:54:52
 Sv76RFsB9y7SL3Pi0yfZSv2polZI6VeU	{"cookie":{"originalMaxAge":604800000,"expires":"2026-04-06T12:44:19.702Z","secure":false,"httpOnly":true,"path":"/"},"userId":"admin","username":"admin"}	2026-04-12 18:26:27
 \.
 
@@ -5453,5 +5453,5 @@ ALTER TABLE ONLY public.work_orders
 -- PostgreSQL database dump complete
 --
 
-\unrestrict 5x1rXJTbLJuKZLQEzvKhokYWNjE5M5uhWbuBfYO0PWwBdiLKsoEheUjYEp6O7sZ
+\unrestrict mR2x61wWI9RreHZsEnKa1agx90wtMuKUmdGhoJTkFWl4EZVdM7JcFcLMehivMcS
 
